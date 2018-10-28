b495c70e92e7c677dc3f4c41376ca190deee7b87
unknown <rohan.kangale2011@gmail.com>
2018-10-28 20:23:53 +0530

    API call for GET updated

--- routes/main.py
+++ routes/main.py
@@ -6,5 +6,5 @@ main = Blueprint('main', __name__)
 @main.route('/')
 def index():
     user_cursor = mongo.db.User.find()
-    user_list = [doc for doc in user_list]
+    user_list = [doc for doc in user_cursor]
     return jsonify(data = user_list)
